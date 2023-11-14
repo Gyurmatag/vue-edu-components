50e390baad26a7123ce324fd17f4d19e725308ad
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-11-14 10:19:58 +0100

    Revert "revert transition"
    This reverts commit 2929c39eb452167f274ef043489b21d58ba7143f.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
       class="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
   >
     <div class="bg-white p-8 rounded-lg shadow-md w-96">
-      <MessageDisplay :message="message" :key="message" />
+      <transition name="fade" mode="out-in">
+        <MessageDisplay :message="message" :key="message" />
+      </transition>
       <ToggleButton @toggle="toggleMessage" />
     </div>
   </div>
@@ -20,3 +22,12 @@ const toggleMessage = () => {
   message.value = message.value === "Hello!" ? "Goodbye!" : "Hello!";
 };
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
